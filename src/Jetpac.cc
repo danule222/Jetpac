5f647f118f8ad48e9258363ddf2aa41c46396d76
Daniel Ramírez Morilla <daniel@xarblanca.es>
2022-02-16 19:09:08 +0100

    Inicializar Jetpac.cc

---
+++ src/Jetpac.cc
@@ -0,0 +1,58 @@
+/**
+ * @file Jetpac.cc
+ *
+ * @brief Primary file of the game where the main function is.
+ */
+
+// Generic includes
+#include <esat/window.h>
+#include <esat/draw.h>
+#include <esat/input.h>
+#include <esat/sprite.h>
+#include <esat/time.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+// Personal includes
+// Common
+
+// Screens
+
+// Functions
+
+// ZX Spectrum res.: 256×192 pixels
+int WIDTH = 768, HEIGHT = 576;
+
+unsigned char fps = 60;
+double current_time, last_time;
+
+int esat::main(int argc, char **argv)
+{
+	srand(time(NULL));
+
+	esat::WindowInit(WIDTH, HEIGHT);
+	WindowSetMouseVisibility(true);
+
+	while (esat::WindowIsOpened() &&
+				 !esat::IsSpecialKeyDown(esat::kSpecialKey_Escape))
+	{
+		last_time = esat::Time();
+		esat::DrawBegin();
+		esat::DrawClear(0, 0, 0);
+
+		esat::DrawEnd();
+
+		// Control FPS
+		do
+		{
+			current_time = esat::Time();
+		} while ((current_time - last_time) <= 1000.0 / fps);
+		esat::WindowFrame();
+	}
+
+	esat::WindowDestroy();
+
+	return 0;
+}
