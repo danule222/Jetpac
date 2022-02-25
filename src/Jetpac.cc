db6e7eebcdbe12ec1adf36d14dc16837a3cab86c
Daniel Ramírez Morilla <daniel@xarblanca.es>
2022-02-25 03:37:16 +0100

    Implementar sistema de sonido

--- src/Jetpac.cc
+++ src/Jetpac.cc
@@ -14,6 +14,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+// SoLoud
+#include <esat_extra/soloud/soloud.h>
+#include <esat_extra/soloud/soloud_wav.h>
 
 // Personal includes
 // Common
@@ -22,6 +25,7 @@
 #include "common/lists_enems.cc"
 #include "common/utils.cc"
 #include "common/math.cc"
+#include "common/sound.cc"
 // Functions
 #include "functions/player.cc"
 #include "functions/enemy.cc"
