ea7e70c3225a9e5b56453a7ce355173be2486bff
jaumebv <jaumebvj@gmail.com>
2022-02-28 03:22:27 +0100

    arregladoxd

--- src/functions/power_ups.cc
+++ src/functions/power_ups.cc
@@ -1,320 +1,282 @@
-const int TotalSpritesPowerUps = 6, TotalObjetos = 6, TotalColores = 12, pantallaxpowerups = 768, posxnavep = 500;
-int caracteristicaspowerup, contadoraparicionpowerups, contadorfuel, contadorfuel2, aparicionYpowerups = 120, totalcaidafuel = 0, colorespunum, contadorcolorfuel;
-float gravedadhola = 1;
-bool mantenerfuel = false, caidaf = false, aparicionfuelPU = false, activarvuelo = false, subirnave = false;
+const int TotalSpritesPowerUps=6,TotalObjetos=6,TotalColores=12,pantallaxpowerups=768,posxnavep=500;
+int caracteristicaspowerup,contadoraparicionpowerups,contadorfuel,contadorfuel2,aparicionYpowerups=120,totalcaidafuel=0,colorespunum,contadorcolorfuel;
+float gravedadhola=1;
+bool mantenerfuel=false,caidaf=false,aparicionfuelPU=false,activarvuelo=false,subirnave=false;
 Color diamantepowerups;
 powerups *objeto;
 esat::SpriteHandle spritefinal;
 esat::SpriteHandle *spritesPowerups;
 
-// Inicializar los sprites de los Power Ups
-void InicializarSpritesPowerUps()
-{
-	// Memoria
-	spritesPowerups = (esat::SpriteHandle *)malloc(6 * sizeof(esat::SpriteHandle));
+
+
+//Inicializar los sprites de los Power Ups
+void InicializarSpritesPowerUps(){
+	//Memoria 
+	spritesPowerups = (esat::SpriteHandle*)malloc( 6 * sizeof(esat::SpriteHandle));
 
 	spritefinal = esat::SpriteFromFile("../assets/spritesheet.png");
-	// Asignar cada imagen a cada sprite
-	int spritenum = 672;
-	for (int i = 0; i < TotalSpritesPowerUps; i++)
-	{
-		*(spritesPowerups + i) = esat::SubSprite(spritefinal, spritenum, 48, 48, 48);
-		spritenum += 48;
+	//Asignar cada imagen a cada sprite
+	int spritenum=672;
+	for(int i=0;i<TotalSpritesPowerUps;i++){
+		*(spritesPowerups + i) = esat::SubSprite(spritefinal,spritenum,48,48,48);
+		spritenum+=48;
 	}
 }
 
-void ColoresPowerUps()
-{
+
+void ColoresPowerUps(){
 
 	switch (colorespunum)
 	{
 	case 1:
-		(objeto + 1)->color = {c_cyan};
+		(objeto+1)->color={c_cyan};
 		break;
 	case 2:
-		(objeto + 1)->color = {c_blue};
+		(objeto+1)->color={c_blue};
 		break;
 	case 3:
-		(objeto + 1)->color = {c_dark_red};
+		(objeto+1)->color={c_dark_red};
 		break;
 	case 4:
-		(objeto + 1)->color = {c_red};
+		(objeto+1)->color={c_red};
 		break;
 	case 5:
-		(objeto + 1)->color = {c_dark_magenta};
+		(objeto+1)->color={c_dark_magenta};
 		break;
 	case 6:
-		(objeto + 1)->color = {c_magenta};
+		(objeto+1)->color={c_magenta};
 		break;
 	case 7:
-		(objeto + 1)->color = {c_dark_green};
+		(objeto+1)->color={c_dark_green};
 		break;
 	case 8:
-		(objeto + 1)->color = {c_green};
+		(objeto+1)->color={c_green};
 		break;
 	case 9:
-		(objeto + 1)->color = {c_dark_cyan};
+		(objeto+1)->color={c_dark_cyan};
 		break;
 	case 10:
-		(objeto + 1)->color = {c_cyan};
+		(objeto+1)->color={c_cyan};
 		break;
 	case 11:
-		(objeto + 1)->color = {c_dark_yellow};
+		(objeto+1)->color={c_dark_yellow};
 		break;
 	case 12:
-		(objeto + 1)->color = {c_yellow};
+		(objeto+1)->color={c_yellow};
 		break;
 	case 13:
-		(objeto + 1)->color = {c_red};
+		(objeto+1)->color={c_red};
 		break;
 	case 14:
-		(objeto + 1)->color = {c_grey};
+		(objeto+1)->color={c_grey};
 		break;
 	case 15:
-		(objeto + 1)->color = {c_white};
-		break;
+		(objeto+1)->color={c_white};
+		break;	
 	}
 }
 
-// Inicializar las características de los Power Ups
-void InicializarPU()
-{
-	// Memoria
-	objeto = (powerups *)malloc((TotalObjetos) * sizeof(powerups));
 
-	// Asignarle a cada PowerUp su numero para posteriormente asignarle a cada uno una caracteristica diferente
-	for (int i = 0; i < TotalObjetos; i++)
-	{
-		(objeto + i)->numero = i;
-		(objeto + i)->estado = false;
+
+//Inicializar las características de los Power Ups
+void InicializarPU(){
+	//Memoria 
+	objeto = (powerups*)malloc((TotalObjetos) * sizeof(powerups));
+
+	//Asignarle a cada PowerUp su numero para posteriormente asignarle a cada uno una caracteristica diferente
+	for(int i=0;i<TotalObjetos;i++){
+		(objeto+i)->numero=i;
+		(objeto+i)->estado=false;
 	}
 
-	// Caracterísiticas de cada objeto
-	for (int i = 0; i < TotalObjetos; i++)
+	//Caracterísiticas de cada objeto
+	for(int i=0;i<TotalObjetos;i++){
+	switch ((objeto+i)->numero)
 	{
-		switch ((objeto + i)->numero)
-		{
-		// Fuel
+		//Fuel
 		case 0:
-			(objeto + i)->score = 250;
-			(objeto + i)->color = c_magenta;
+			(objeto+i)->score=250;
+      (objeto+i)->color=c_magenta;
 			break;
-		// Diamante
+		//Diamante
 		case 1:
-			(objeto + i)->score = 250;
-			(objeto + i)->color = c_dark_blue;
+			(objeto+i)->score=250;
+      (objeto+i)->color=c_dark_blue;
 			break;
-		// Slime raro
+		//Slime raro
 		case 2:
-			(objeto + i)->score = 250;
-			(objeto + i)->color = c_green;
+			(objeto+i)->score=250;
+      (objeto+i)->color=c_green;
 			break;
-		// Nuclear
+		//Nuclear
 		case 3:
-			(objeto + i)->score = 250;
-			(objeto + i)->color = c_cyan;
+			(objeto+i)->score=250;
+      (objeto+i)->color=c_cyan;
 			break;
-		// Tres Bolas Unidas Por Una Raya (???)
+		//Tres Bolas Unidas Por Una Raya (???)
 		case 4:
-			(objeto + i)->score = 250;
-			(objeto + i)->color = c_cyan;
+			(objeto+i)->score=250;
+      (objeto+i)->color=c_cyan;
 			break;
-		// Bloque De Oro
+		//Bloque De Oro
 		case 5:
-			(objeto + i)->score = 300;
-			(objeto + i)->color = c_yellow;
+			(objeto+i)->score=300;
+      (objeto+i)->color=c_yellow;
 			break;
 		}
 	}
 }
 
-void GravedadPowerUp()
-{
-	// El PowerUp cae si no detecta colisiones
-	for (int i = 0; i < TotalObjetos; i++)
-	{
-		if ((objeto + i)->estado)
-		{
-			if (!(CheckCollision((objeto + i)->pos.x, (objeto + i)->pos.y, 48, 48, 0, 552, 768, 24) ||
-						CheckCollision((objeto + i)->pos.x, (objeto + i)->pos.y, 48, 48, 96, 216, 144, 24) ||
-						CheckCollision((objeto + i)->pos.x, (objeto + i)->pos.y, 48, 48, 360, 288, 96, 24) ||
-						CheckCollision((objeto + i)->pos.x, (objeto + i)->pos.y, 48, 48, 576, 144, 144, 24)))
-			{
-				(objeto + i)->pos.y += gravedadhola;
+
+
+void GravedadPowerUp(){
+	//El PowerUp cae si no detecta colisiones
+	for(int i=0;i<TotalObjetos;i++){
+		if((objeto+i)->estado){
+			if(!(CheckCollision((objeto+i)->pos.x,(objeto+i)->pos.y,48,48,0,552,768,24) ||
+			 CheckCollision((objeto+i)->pos.x,(objeto+i)->pos.y,48,48,96,216,144,24) ||
+				CheckCollision((objeto+i)->pos.x,(objeto+i)->pos.y,48,48,360,288,96,24) || 
+				CheckCollision((objeto+i)->pos.x,(objeto+i)->pos.y,48,48,576,144,144,24))){
+				(objeto+i)->pos.y+=gravedadhola;
 			}
 		}
 	}
 }
+
+
+
 
 // {(objeto+i)->pos.x,(objeto+i)->pos.y}
 
-void PowerUpsDraw()
-{
-	// Dibujar
-	for (int i = 0; i < TotalObjetos; i++)
-	{
-		if ((objeto + i)->estado)
-		{
-			if ((objeto + i)->numero == 1 || (objeto + i)->numero == 5)
-			{
-				if ((objeto + i)->numero == 1)
-				{
-					contadorcolorfuel++;
-					if (contadorcolorfuel > 450)
-						contadorcolorfuel = 0;
-					if (colorespunum > 15)
-						colorespunum = 0;
-					if (contadorcolorfuel % 5 == 0)
-						colorespunum++;
-					DrawColorSquare((objeto + i)->pos, (objeto + i)->color, 48, 48, true);
-					esat::DrawSprite(*(spritesPowerups + i), (objeto + i)->pos.x, (objeto + i)->pos.y);
-				}
-				if ((objeto + i)->numero == 5)
-				{
-					DrawColorSquare((objeto + i)->pos, (objeto + i)->color, 48, 24, true);
-					esat::DrawSprite(*(spritesPowerups + i), (objeto + i)->pos.x, (objeto + i)->pos.y);
-				}
-			}
-			else
-			{
-				DrawColorSquare((objeto + i)->pos, (objeto + i)->color, 48, 48, true);
-				esat::DrawSprite(*(spritesPowerups + i), (objeto + i)->pos.x, (objeto + i)->pos.y);
+void PowerUpsDraw(){
+	//Dibujar
+	for(int i=0;i<TotalObjetos;i++) {
+    	if((objeto+i)->estado){
+				if((objeto+i)->numero==1 || (objeto+i)->numero==5){
+					if((objeto+i)->numero==1){
+						contadorcolorfuel++;
+						if(contadorcolorfuel>450)contadorcolorfuel=0;
+						if(colorespunum>15)colorespunum=0;
+						if(contadorcolorfuel%5==0)colorespunum++;
+					DrawColorSquare((objeto+i)->pos,(objeto+i)->color,48,48,true);
+					esat::DrawSprite(*(spritesPowerups+i),(objeto+i)->pos.x,(objeto+i)->pos.y);
+					}
+					if((objeto+i)->numero==5){
+					DrawColorSquare((objeto+i)->pos,(objeto+i)->color,48,24,true);
+					esat::DrawSprite(*(spritesPowerups+i),(objeto+i)->pos.x,(objeto+i)->pos.y);
+					}
+			}else{
+					DrawColorSquare((objeto+i)->pos,(objeto+i)->color,48,48,true);
+					esat::DrawSprite(*(spritesPowerups+i),(objeto+i)->pos.x,(objeto+i)->pos.y);
 			}
 		}
 	}
 }
 
-void AparicionPowerUps()
-{
-	// Comprueba el num de PowerUps en pantalla
-	int vivo = 0;
-	for (int i = 1; i < TotalObjetos; i++)
-	{
-		if ((objeto + i)->estado)
-			vivo++;
-	}
 
-	// Si no hay ninguno en pantalla, hace spawnear a un PowerUp
-	if (vivo < 1 && totalcaidafuel < 121)
-	{
+void AparicionPowerUps(){
+	//Comprueba el num de PowerUps en pantalla
+	int vivo=0;
+	for(int i=1;i<TotalObjetos;i++){
+		if((objeto+i)->estado)vivo++;
+	}
+	
+	//Si no hay ninguno en pantalla, hace spawnear a un PowerUp
+	if(vivo<1 && totalcaidafuel<121){
 		contadoraparicionpowerups++;
 
-		if (contadoraparicionpowerups > (rand() % 1000) + 200)
-		{
-			int aux = rand() % (TotalObjetos - 1);
-			(objeto + aux + 1)->pos.x = rand() % (pantallaxpowerups - 48);
-			(objeto + aux + 1)->pos.y = aparicionYpowerups;
-			(objeto + aux + 1)->estado = true;
-			contadoraparicionpowerups = 0;
+		if(contadoraparicionpowerups>(rand()%1000)+200){
+			int aux=rand()%(TotalObjetos-1);
+			(objeto+aux+1)->pos.x=rand()%(pantallaxpowerups-48);
+			(objeto+aux+1)->pos.y=aparicionYpowerups;
+			(objeto+aux+1)->estado=true;
+			contadoraparicionpowerups=0;
 		}
+
 	}
 
-	// Aparición del fuel por separado
-	if (!(objeto)->estado)
-	{
+	//Aparición del fuel por separado
+	if(!(objeto)->estado){
 		contadorfuel++;
-		if (contadorfuel > 121)
-			contadorfuel = 0;
-		if (contadorfuel > 120 && aparicionfuelPU && totalcaidafuel < 121)
-		{
-			(objeto)->estado = true;
-			// Margen de aparición del powerup respecto a la x de la nave
-			int div2 = rand() % 2;
-			if (div2 == 1 || div2 == 2)
-			{
-				(objeto)->pos.x = rand() % 475;
+		if(contadorfuel>121)contadorfuel=0;
+		if(contadorfuel>120 && aparicionfuelPU && totalcaidafuel<121){
+			(objeto)->estado=true;
+			//Margen de aparición del powerup respecto a la x de la nave
+			int div2=rand()%2;
+			if(div2==1 || div2==2){
+				(objeto)->pos.x=rand()%475;
+			}else{
+				(objeto)->pos.x=(700-(rand()%150));
 			}
-			else
-			{
-				(objeto)->pos.x = (700 - (rand() % 150));
-			}
-			(objeto)->pos.y = aparicionYpowerups;
+			(objeto)->pos.y=aparicionYpowerups;
 		}
 	}
 }
 
-void DesaparicionPowerups(float posjugx, float posjugy)
-{
-	// Hacer desaparecer los powerups al tener contacto con el jugador
-	for (int i = 1; i < TotalObjetos; i++)
-	{
-		if ((objeto + i)->estado)
-		{
-			if (CheckCollision((objeto + i)->pos.x, (objeto + i)->pos.y, 48, 48, posjugx, posjugy, 48, 72))
-			{
-				(objeto + i)->estado = false;
+void DesaparicionPowerups(float posjugx,float posjugy){
+	//Hacer desaparecer los powerups al tener contacto con el jugador
+	for(int i=1;i<TotalObjetos;i++){
+		if((objeto+i)->estado){
+			if(CheckCollision((objeto+i)->pos.x,(objeto+i)->pos.y,48,48,posjugx,posjugy,48,72)){
+				g_player.score+=(objeto+i)->score;
+				(objeto+i)->estado=false;
 			}
 		}
 	}
 }
 
-bool RecogerFuel(float posjugx, float posjugy)
-{
-	if ((objeto)->estado)
-	{
-		if (CheckCollision((objeto)->pos.x, (objeto)->pos.y, 48, 48, posjugx, posjugy, 48, 72))
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	else
-	{
+bool RecogerFuel(float posjugx, float posjugy){
+	if((objeto)->estado){
+		if(CheckCollision((objeto)->pos.x,(objeto)->pos.y,48,48,posjugx,posjugy,48,72)){
+				return true;
+			}else{
+				return false;
+			}
+	}else{
 		return false;
 	}
 }
 
-void MovimientoFuel(float posjugx, float posjugy)
-{
-	if (RecogerFuel(posjugx, posjugy) && !caidaf)
-		mantenerfuel = true;
+void MovimientoFuel(float posjugx,float posjugy){
+	if(RecogerFuel(posjugx,posjugy)&&!caidaf)mantenerfuel=true;
 
-	if (mantenerfuel)
-	{
-		if (contadorfuel2 > 100)
-			contadorfuel2 = 0;
-		contadorfuel2++;
-		if (contadorfuel2 % 5 == 0)
-		{
-			(objeto)->pos.x = posjugx - 10;
-			(objeto)->pos.y = posjugy + 24;
+	if(mantenerfuel){
+		if(contadorfuel2>100)contadorfuel2=0;
+		contadorfuel2++;	
+		if(contadorfuel2%5==0){
+			(objeto)->pos.x=posjugx-10;
+			(objeto)->pos.y=posjugy+24;
+		}
+	}
+}
+	
+	
+void CaidaFuel(){
+if((objeto)->pos.x>(486) && (objeto)->pos.x<(522)){
+		caidaf=true;
+		mantenerfuel=false;
+		(objeto)->pos.x=504;
+		(objeto)->pos.y+=gravedadhola;
+
+		if((objeto)->pos.x==504 && (objeto)->pos.y>=504){
+			if((objeto)->estado)totalcaidafuel+=24;
+			(objeto)->estado=false;
+			caidaf=false;
 		}
 	}
 }
 
-void CaidaFuel()
-{
-	if ((objeto)->pos.x > (486) && (objeto)->pos.x < (522))
-	{
-		caidaf = true;
-		mantenerfuel = false;
-		(objeto)->pos.x = 504;
-		(objeto)->pos.y += gravedadhola;
-
-		if ((objeto)->pos.x == 504 && (objeto)->pos.y >= 504)
-		{
-			if ((objeto)->estado)
-				totalcaidafuel += 24;
-			(objeto)->estado = false;
-			caidaf = false;
+	void ShipVolandop(float posjugx, float posjugy){
+		if(totalcaidafuel>120 && CheckCollision(504,504,48,144,posjugx,posjugy,48,72) && g_player.alive){
+			g_player.alive=false;
+			subirnave=true;
 		}
 	}
-}
+		
 
-void ShipVolandop(float posjugx, float posjugy)
-{
-	if (totalcaidafuel > 120 && CheckCollision(504, 504, 48, 144, posjugx, posjugy, 48, 72) && g_player.alive)
-	{
-		g_player.alive = false;
-		subirnave = true;
-	}
-}
 
-void MovementsPowerUps()
-{
+
+void MovementsPowerUps(){
 	ColoresPowerUps();
 	AparicionPowerUps();
 	GravedadPowerUp();
@@ -324,19 +286,18 @@ void MovementsPowerUps()
 	ShipVolandop(g_player.pos.x, g_player.pos.y);
 }
 
-void PowerUpsStart()
-{
+
+
+void PowerUpsStart(){
 	InicializarPU();
 	InicializarSpritesPowerUps();
 }
 
-void PowerUpsEnd()
-{
+void PowerUpsEnd(){
 
-	for (int i = 0; i < TotalSpritesPowerUps; i++)
-	{
-		esat::SpriteRelease(*(spritesPowerups + i));
+	for(int i=0;i<TotalSpritesPowerUps;i++){
+		esat::SpriteRelease(*(spritesPowerups+i));
 	}
 	free(objeto);
-	free(spritesPowerups);
+  free(spritesPowerups);
 }
--- src/functions/ship.cc
+++ src/functions/ship.cc
@@ -1,259 +1,240 @@
-
-const int TotalSpritesNaves = 12, PosicionNaveFija = 768;
-int gravedadhola2 = 1, contadornave2, numerodegasofa;
-bool empezarnivelnave = true, ocupadonave = false, bajarnave;
+const int TotalSpritesNaves=12,PosicionNaveFija=768;
+int gravedadhola2=1,contadornave2,numerodegasofa,contadorfuegonave,contadorparpadeo;
+bool empezarnivelnave=true, ocupadonave=false,bajarnave,fuegonave=false;
 
 NaveJetpac *Nave;
 esat::SpriteHandle *spritesNave;
+esat::SpriteHandle spritehola1;
+esat::SpriteHandle spritehola2;
 
-// Inicializar los sprites de la nave
-void InicializarSpritesNave()
-{
-	// Memoria
-	spritesNave = (esat::SpriteHandle *)malloc(TotalSpritesNaves * sizeof(esat::SpriteHandle));
+//Inicializar los sprites de la nave
+void InicializarSpritesNave(){
+	//Memoria 
+	spritesNave = (esat::SpriteHandle*)malloc( TotalSpritesNaves * sizeof(esat::SpriteHandle));
 
 	spritefinal = esat::SpriteFromFile("../assets/spritesheet.png");
 
-	// Asignar cada imagen a cada sprite
-	int spritenum = 0;
-	for (int i = 0; i < TotalSpritesNaves; i++)
-	{
-		*(spritesNave + i) = esat::SubSprite(spritefinal, spritenum, 0, 48, 48);
-		spritenum += 48;
+	//Asignar cada imagen a cada sprite
+	int spritenum=0;
+	for(int i=0;i<TotalSpritesNaves;i++){
+		*(spritesNave + i) = esat::SubSprite(spritefinal,spritenum,0,48,48);
+		spritenum+=48;
 	}
+	spritehola1=esat::SubSprite(spritefinal,864,0,48,48);
+	spritehola2=esat::SubSprite(spritefinal,912,0,48,48);
 }
 
-void InicializarNaveJP()
-{
-	// Memoria
-	Nave = (NaveJetpac *)malloc(TotalSpritesNaves * sizeof(NaveJetpac));
-	int cada3 = 0;
-	for (int i = 0; i < TotalSpritesNaves; i++)
-	{
-		(Nave + i)->estado = false;
-		(Nave + i)->mantenernave = false;
-		(Nave + i)->caidan = false;
-		(Nave + i)->posicionfinal = false;
+void InicializarNaveJP(){
+	//Memoria 
+	Nave = (NaveJetpac*)malloc(TotalSpritesNaves * sizeof(NaveJetpac));
+	int cada3=0;
+	for(int i=0;i<TotalSpritesNaves;i++){
+		(Nave+i)->estado=false;
+		(Nave+i)->mantenernave=false;
+		(Nave+i)->caidan=false;
+		(Nave+i)->posicionfinal=false;
 		cada3++;
-		if (cada3 == 3)
-		{
-			(Nave + i)->posicionfinal = true;
-			cada3 = 0;
+		if(cada3==3){
+			(Nave+i)->posicionfinal=true;
+			cada3=0;
 		}
 	}
 }
 
-void AparicionNave(int nivel)
-{
-	// Dependiendo del nivel, asigna una posición a la nave
-	if (empezarnivelnave)
-	{
-		if (nivel % 4 == 0)
-		{
-			(Nave + 0 + ((nivel / 4) * 3))->pos.x = 168;
-			(Nave + 0 + ((nivel / 4) * 3))->pos.y = 192;
-			(Nave + 0 + ((nivel / 4) * 3))->estado = true;
-			printf("%d ", (0 + ((nivel / 4) * 3)));
-			(Nave + 1 + ((nivel / 4) * 3))->pos.x = 300;
-			(Nave + 1 + ((nivel / 4) * 3))->pos.y = 300;
-			(Nave + 1 + ((nivel / 4) * 3))->estado = true;
-			printf("%d ", (1 + ((nivel / 4) * 3)));
 
-			(Nave + 2 + ((nivel / 4) * 3))->pos.x = 504;
-			(Nave + 2 + ((nivel / 4) * 3))->pos.y = 504;
-			(Nave + 2 + ((nivel / 4) * 3))->estado = true;
-			printf("%d \n", (2 + ((nivel / 4) * 3)));
+
+void AparicionNave(int nivel){
+	//Dependiendo del nivel, asigna una posición a la nave
+	if(empezarnivelnave){
+		if(nivel%4==0){
+			(Nave+0+((nivel/4) * 3))->pos.x=168;
+			(Nave+0+((nivel/4) * 3))->pos.y=192;
+			(Nave+0+((nivel/4) * 3))->estado=true;
+
+			(Nave+1+((nivel/4) * 3))->pos.x=300;
+			(Nave+1+((nivel/4) * 3))->pos.y=300;
+			(Nave+1+((nivel/4) * 3))->estado=true;
+
+			(Nave+2+((nivel/4) * 3))->pos.x=504;
+			(Nave+2+((nivel/4) * 3))->pos.y=504;
+			(Nave+2+((nivel/4) * 3))->estado=true;
 		}
-		empezarnivelnave = false;
+		empezarnivelnave=false;
 	}
 }
 
-bool RecogerNave(int numnave, float posjugx, float posjugy)
-{
-	if (CheckCollision((Nave + numnave)->pos.x, (Nave + numnave)->pos.y, 48, 48, posjugx, posjugy, 48, 72))
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
 
-void ShipDraw()
-{
-	for (int i = 0; i < TotalSpritesNaves; i++)
-	{
-		if ((Nave + i)->estado)
-		{
-			DrawColorSquare((Nave + i)->pos, c_white, 48, 48, true);
-			if (i == 2 || i == 5 || i == 8 || i == 11)
-			{
-				esat::Vec2 naveposxy = {504, (Nave + i)->pos.y + 48};
-				DrawColorSquare(naveposxy, c_magenta, 48, -totalcaidafuel);
+
+bool RecogerNave(int numnave,float posjugx, float posjugy){
+		if(CheckCollision((Nave+numnave)->pos.x,(Nave+numnave)->pos.y,48,48,posjugx,posjugy,48,72)){
+				return true;
+			}else{
+				return false;
 			}
+	}
+
+void ShipDraw(){
+for(int i=0;i<TotalSpritesNaves;i++) {
+    	if((Nave+i)->estado){
+				DrawColorSquare((Nave+i)->pos,c_white,48,48,true);
+				if(i==2 || i==5 || i==8 || i==11){
+					esat::Vec2 naveposxy={504,(Nave+i)->pos.y+48};
+					if(totalcaidafuel==144){
+						contadorparpadeo++;
+						if(contadorparpadeo>60)contadorparpadeo=0;
+						if(contadorparpadeo>=30)DrawColorSquare(naveposxy,c_magenta,48,-totalcaidafuel);
+						if(contadorparpadeo<30)DrawColorSquare(naveposxy,c_white,48,-totalcaidafuel);
+					}else{
+					DrawColorSquare(naveposxy,c_magenta,48,-totalcaidafuel);
+					}
+				}
 		}
 	}
-	for (int i = 0; i < TotalSpritesNaves; i++)
-	{
-		if ((Nave + i)->estado)
-		{
-			esat::DrawSprite(*(spritesNave + i), (Nave + i)->pos.x, (Nave + i)->pos.y);
+	for(int i=0;i<TotalSpritesNaves;i++) {
+    	if((Nave+i)->estado){
+				esat::DrawSprite(*(spritesNave+i),(Nave+i)->pos.x,(Nave+i)->pos.y);
+		}
+	}
+	if(fuegonave){
+		for(int i=0;i<TotalSpritesNaves;i++) {
+    	if((Nave+i)->estado){
+				if(i==2 || i==5 || i==8 || i==11){
+					contadorfuegonave++;
+					if(contadorfuegonave>6)contadorfuegonave=0;
+					int hola;
+					esat::Vec2 naveposxy={504,(Nave+i)->pos.y+72};
+					DrawColorSquare(naveposxy,c_red,48,48,true);
+					if(contadorfuegonave>3)esat::DrawSprite(spritehola1,(Nave+i)->pos.x,(Nave+i)->pos.y+72);
+					if(contadorfuegonave<=3)esat::DrawSprite(spritehola2,(Nave+i)->pos.x,(Nave+i)->pos.y+72);
+				}
+		}
+	}
+	}
+}
+
+
+void MovimientoNave(float posjugx,float posjugy){
+	for(int i=0;i<TotalSpritesNaves;i++){
+		//Revisa si esta vivo
+		if((Nave+i)->estado && !(Nave+i)->posicionfinal && (Nave+i+1)->posicionfinal){
+			//Revisa si hay colision y si no hay ninguna nave cayendo
+			if(RecogerNave(i,posjugx,posjugy)&&!(Nave+i)->caidan)(Nave+i)->mantenernave=true;
+			if((Nave+i)->mantenernave){
+				//Contador para darle efecto de retraso mientras sigue al jugador la nave
+				if(contadornave2>100)contadornave2=0;
+				contadornave2++;	
+				if(contadornave2%5==0){
+					(Nave+i)->pos.x=posjugx;
+					(Nave+i)->pos.y=posjugy+24;
+				}
+		  }
 		}
 	}
 }
 
-void MovimientoNave(float posjugx, float posjugy)
-{
-	for (int i = 0; i < TotalSpritesNaves; i++)
-	{
-		// Revisa si esta vivo
-		if ((Nave + i)->estado && !(Nave + i)->posicionfinal && (Nave + i + 1)->posicionfinal)
-		{
-			// Revisa si hay colision y si no hay ninguna nave cayendo
-			if (RecogerNave(i, posjugx, posjugy) && !(Nave + i)->caidan)
-				(Nave + i)->mantenernave = true;
-			if ((Nave + i)->mantenernave)
-			{
-				// Contador para darle efecto de retraso mientras sigue al jugador la nave
-				if (contadornave2 > 100)
-					contadornave2 = 0;
-				contadornave2++;
-				if (contadornave2 % 5 == 0)
-				{
-					(Nave + i)->pos.x = posjugx;
-					(Nave + i)->pos.y = posjugy + 24;
+void NaveCayendo(){
+	for(int i=0;i<TotalSpritesNaves;i++){
+		if((Nave+i)->pos.x>(486) && (Nave+i)->pos.x<(522) && !(Nave+i)->posicionfinal && (Nave+i)->pos.y<408){
+			(Nave+i)->caidan=true;
+			(Nave+i)->mantenernave=false;
+			(Nave+i)->pos.x=504;
+		}
+    if((Nave+i)->pos.x==504 && (Nave+i)->posicionfinal){
+				(Nave+i)->caidan=false;
+			}
+    if((Nave+i)->caidan)(Nave+i)->pos.y+=gravedadhola2;
+	}
+}
+
+void PosFinalNave(int nivel){
+	float posy=408;
+	float posy2=407;
+	for(int i=0;i<2;i++){
+		if((Nave+i+((nivel/4) * 3))->pos.y>=posy2 && (Nave+i+((nivel/4) * 3))->pos.y<=posy && (Nave+i+((nivel/4) * 3))->pos.x==504){
+			(Nave+i+((nivel/4) * 3))->posicionfinal=true;
+		}
+		posy+=48;
+		posy2+=48;
+	}
+	if((Nave+((nivel/4) * 3))->posicionfinal){
+		aparicionfuelPU=true;
+	}
+}
+
+void NaveDespegandop(){
+	if(subirnave){
+		fuegonave=true;
+			for(int i=0;i<12;i++){
+				if((Nave+i)->estado)(Nave+i)->pos.y--; 
+				if((Nave+i)->pos.y<-20 && (Nave+i)->estado){
+					fuegonave=false;
+					bajarnave=true;
+					if(g_player.nivel==3){
+					(Nave+0)->estado=false;
+					(Nave+1)->estado=false;
+					(Nave+2)->estado=false;
+					g_player.alive=true;
+					empezarnivelnave=true;
+					bajarnave=false;
+					}
+					if(g_player.nivel==7){
+					(Nave+3)->estado=false;
+					(Nave+4)->estado=false;
+					(Nave+5)->estado=false;
+					g_player.alive=true;
+					empezarnivelnave=true;
+					bajarnave=false;
+					}
+					if(g_player.nivel==11){
+					(Nave+6)->estado=false;
+					(Nave+7)->estado=false;
+					(Nave+8)->estado=false;
+					g_player.alive=true;
+					empezarnivelnave=true;
+					bajarnave=false;
+					}
+					g_player.nivel++;
+					totalcaidafuel=0;
+					subirnave=false;
 				}
 			}
 		}
-	}
-}
-
-void NaveCayendo()
-{
-	for (int i = 0; i < TotalSpritesNaves; i++)
-	{
-		if ((Nave + i)->pos.x > (486) && (Nave + i)->pos.x < (522) && !(Nave + i)->posicionfinal && (Nave + i)->pos.y < 408)
-		{
-			(Nave + i)->caidan = true;
-			(Nave + i)->mantenernave = false;
-			(Nave + i)->pos.x = 504;
-		}
-		if ((Nave + i)->pos.x == 504 && (Nave + i)->posicionfinal)
-		{
-			(Nave + i)->caidan = false;
-		}
-		if ((Nave + i)->caidan)
-			(Nave + i)->pos.y += gravedadhola2;
-	}
-}
-
-void PosFinalNave(int nivel)
-{
-	float posy = 408;
-	float posy2 = 407;
-	for (int i = 0; i < 2; i++)
-	{
-		if ((Nave + i + ((nivel / 4) * 3))->pos.y >= posy2 && (Nave + i + ((nivel / 4) * 3))->pos.y <= posy && (Nave + i + ((nivel / 4) * 3))->pos.x == 504)
-		{
-			(Nave + i + ((nivel / 4) * 3))->posicionfinal = true;
-		}
-		posy += 48;
-		posy2 += 48;
-	}
-	if ((Nave + ((nivel / 4) * 3))->posicionfinal)
-	{
-		aparicionfuelPU = true;
-	}
-}
-
-void NaveDespegandop()
-{
-	if (subirnave)
-	{
-		for (int i = 0; i < 12; i++)
-		{
-			if ((Nave + i)->estado)
-				(Nave + i)->pos.y--;
-			if ((Nave + i)->pos.y < -20 && (Nave + i)->estado)
-			{
-				bajarnave = true;
-				if (g_player.nivel == 3)
-				{
-					(Nave + 0)->estado = false;
-					(Nave + 1)->estado = false;
-					(Nave + 2)->estado = false;
-					g_player.alive = true;
-					empezarnivelnave = true;
-					bajarnave = false;
+//bajar nave
+	if(bajarnave){
+		for(int i=0;i<12;i++){
+				if((Nave+i)->estado)(Nave+i)->pos.y++; 
+				if((Nave+i)->pos.y>504){
+					g_player.alive=true;
+					empezarnivelnave=true;
+					bajarnave=false;
 				}
-				if (g_player.nivel == 7)
-				{
-					(Nave + 3)->estado = false;
-					(Nave + 4)->estado = false;
-					(Nave + 5)->estado = false;
-					g_player.alive = true;
-					empezarnivelnave = true;
-					bajarnave = false;
-				}
-				if (g_player.nivel == 11)
-				{
-					(Nave + 6)->estado = false;
-					(Nave + 7)->estado = false;
-					(Nave + 8)->estado = false;
-					g_player.alive = true;
-					empezarnivelnave = true;
-					bajarnave = false;
-				}
-				g_player.nivel++;
-				totalcaidafuel = 0;
-				subirnave = false;
 			}
-		}
-	}
-
-	if (bajarnave)
-	{
-		for (int i = 0; i < 12; i++)
-		{
-			if ((Nave + i)->estado)
-				(Nave + i)->pos.y++;
-			if ((Nave + i)->pos.y > 504)
-			{
-				g_player.alive = true;
-				empezarnivelnave = true;
-				bajarnave = false;
-			}
-		}
 	}
 }
 
-void ControlarNiveles()
-{
+void ControlarNiveles(){
+
 }
 
-void ShipStart()
-{
+void ShipStart(){
 	InicializarNaveJP();
 	InicializarSpritesNave();
 }
 
-void MovementsShip()
-{
+
+void MovementsShip(){
 	AparicionNave(g_player.nivel);
-	MovimientoNave(g_player.pos.x, g_player.pos.y);
+	MovimientoNave(g_player.pos.x,g_player.pos.y);
 	NaveCayendo();
 	NaveDespegandop();
 	PosFinalNave(g_player.nivel);
 	ControlarNiveles();
 }
 
-void ShipEnd()
-{
-	for (int i = 0; i < TotalSpritesNaves; i++)
-	{
-		esat::SpriteRelease(*(spritesNave + i));
+void ShipEnd(){
+	for(int i=0;i<TotalSpritesNaves;i++){
+		esat::SpriteRelease(*(spritesNave+i));
 	}
 	free(Nave);
-	free(spritesNave);
+  free(spritesNave);
 }
